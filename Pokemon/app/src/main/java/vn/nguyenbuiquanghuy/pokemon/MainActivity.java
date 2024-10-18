ee3e0e964df82dc7f373b55a480d96f8456ceaed
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-10-18 16:29:55 +0700

    Pokemon

---
+++ Pokemon/app/src/main/java/vn/nguyenbuiquanghuy/pokemon/MainActivity.java
@@ -0,0 +1,33 @@
+package vn.nguyenbuiquanghuy.pokemon;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void getControl(){
+        EditText editTextName=findViewById(R.id.txtName);
+        Button buttonTT=findViewById(R.id.btnPokemon);
+        TextView textViewKQ=findViewById(R.id.textViewKQ);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        getControl();
+    }
+}
