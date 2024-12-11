83f7d5176eab824963b77275669704028a7e1d29
NguyenBuiQuanqHuy <2k4quanghuy@gmail.com>
2024-12-11 19:59:50 +0700

    Updated

---
+++ TH_QuizAppFullCode/app/src/main/java/th/nguyenbuiquanghuy/th_quizappfullcode/MainActivity.java
@@ -0,0 +1,52 @@
+package th.nguyenbuiquanghuy.th_quizappfullcode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    CardView cardViewC,cardViewJava,cardViewPython;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        cardViewC=findViewById(R.id.layoutC);
+        cardViewJava=findViewById(R.id.layoutJava);
+        cardViewPython=findViewById(R.id.layoutPython);
+
+        cardViewC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, CPlus.class);
+                startActivity(intent);
+            }
+        });
+
+        cardViewJava.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, Java.class);
+                startActivity(intent);
+            }
+        });
+
+        cardViewPython.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, Python.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ TH_QuizAppFullCode/app/src/main/java/th/nguyenbuiquanghuy/th_quizappfullcode/Python.java
@@ -0,0 +1,22 @@
+package th.nguyenbuiquanghuy.th_quizappfullcode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Python extends AppCompatActivity {
+
+    TextView answerA,answerB,answerC,answerD;
+    Button btnNext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_python);
+    }
+}
